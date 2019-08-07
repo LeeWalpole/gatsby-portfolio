0c6eb881ed9bbe98edb63b34519d741859d796d6
Lee Walpole <lee.walpole@gmail.com>
2019-08-07 21:36:15 +0700

    contact not working yet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import picLogo from '../images/lw-white.png'
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
+
         <div className="logo">
-            <span className="icon fa-diamond"></span>
+            <span className="icon">
+            <span className="image main"><img src={picLogo} alt="" /></span>
+            </span>
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Web Design, Optimisation & SEO</h1>
+                <p>I'm a copywriter / coder who loves flirting with Google and building beautiful, blazingly fast websites.</p>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('skills')}}>Skills</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('experience')}}>Experience</a></li>
+                {/* <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li> */}
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import pic01 from '../images/pic01.jpg'
-import pic02 from '../images/pic02.jpg'
+
 import pic03 from '../images/pic03.jpg'
 
 class Main extends React.Component {
@@ -13,19 +12,76 @@ class Main extends React.Component {
     return (
       <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <article id="intro" className={`${this.props.article === 'skills' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Skills</h2>
+          <div>
+<section className="hal">
+<ul className="small_li_list">
+<li>HTML / CSS</li>
+<li>Javascript</li>
+<li>Adobe Creative Suite</li>
+<li>Wordpress / PHP</li>
+<li>Copywriting</li>
+</ul>
+</section>
+<hr></hr>
+<section className="haf">
+<h2 className="major">Learning</h2>
+<ul>
+<li>React / JSX</li>
+<li>Gatsby</li>
+</ul>
+</section>
+</div>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+        <article id="work" className={`${this.props.article === 'experience' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+          <h2 className="major">Experience</h2>
+          <div>
+
+<section>
+<div className="half">
+<strong>The Sun / News UK</strong>
+<small>2006-2019</small>
+</div>
+<p className="half">
+<small>Reference:</small>
+<small><a href="mailto:david.ross@thesun.co.uk">David Ross</a> (Head of Video)</small>
+</p>
+
+<ul className="cols2 small">
+<li>Developed web tools for editorial teams</li>
+<li>SEO strategy and marketing campaigns</li>
+<li>Wrote features, articles and video scripts</li>
+<li>Edit / produced videos and graphics</li>
+<li>Audience growth / brand awareness</li>
+<li>Managed breaking news on the video desk</li>
+<li>CMS Migration (Wordpress)</li>
+</ul>
+
+
+
+
+</section>
+
+
+
+<h2 className="major">Recent Projects</h2>
+<ul className="cols2 small">
+<li><a href="https://www.phuketcleanse.com" target="_blank">Phuket Cleanse</a><small className="col-light">Branding / Website</small></li>
+<li><a href="https://www.ladsholidayguide.com" target="_blank">Lads Holiday Guide</a><small className="col-light">Editor / Facebook</small></li>
+</ul>
+
+<h2 className="major">Current Projects</h2>
+
+<ol className="cols3 small">
+<li><a href="https://www.nanaretreats.com/" target="_blank">NanaRetreats</a></li>
+<li><a href="https://www.inspiredcapitalinvestments.com" target="_blank">Inspired Investments</a></li>
+<li><a href="https://www.ladsholidayguide.com" target="_blank">Phuket Clenase</a></li>
+</ol>
+
+</div>
           {close}
         </article>
 
@@ -57,10 +113,10 @@ class Main extends React.Component {
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li><a href="https://www.twitter.com/Lee_Walpole/" className="icon fa-twitter" target="_blank"><span className="label">Twitter</span></a></li>
+            <li><a href="https://www.facebook.com/LeeWalpole/" className="icon fa-facebook" target="_blank"><span className="label">Facebook</span></a></li>
+            <li><a href="https://www.instagram.com/LeeWalpole/" className="icon fa-instagram" target="_blank"><span className="label">Instagram</span></a></li>
+            <li><a href="https://www.linkedin.com/in/leewalpole/" className="icon fa-linkedin" target="_blank"><span className="label">LinkedIn</span></a></li>
           </ul>
           {close}
         </article>
